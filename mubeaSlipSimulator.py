2385b2d19ba119f1293e1048ee4541bbea5da7e3
julius <ollesch.julius@fh-swf.de>
2023-01-06 11:21:18 +0100

    All threaded and central measurement store

--- mubeaSlipSimulator.py
+++ mubeaSlipSimulator.py
@@ -1,28 +1,18 @@
-import numpy as np
-from datetime import datetime
-from threading import Timer
-from threading import Lock
-import redis_connector as connector
-import time
-import plotext as plt
 import os
+import time
+from datetime import datetime
+from threading import Timer, Thread
 
-###connection parameters###
-host = "127.0.0.1"
-port = 80
-###end - connection parameters###
+import numpy as np
+import plotext as plt
+
+import redis_connector as connector
 
 ### definitions
 mean_velocity_f = 0.32  # mean velocity of the rolled material exiting the process in m/s
 mean_velocity_r = 0.3  # max velocity of the roll in m/s
 stream_name = "mubea_trb"
 
-###global list of measurements
-measurements = []
-
-client = connector.connect()
-with client:
-    client.ping()
 
 class Measurement:
     def __init__(self, f: float, r: float):
@@ -30,84 +20,126 @@ class Measurement:
         self.velocity_r = r
         self.date = datetime.now()
 
-        global measurements
-        lock = Lock()
-        with lock:
-            measurements.append(self)
-            self.storeInRedis()
     def __str__(self):
-        return "Measurement taken " + self.date.strftime("%m/%d/%Y, %H:%M:%S:%f") + " - velocity_f: " + str(
-            self.velocity_f) + " - velocity_r: " + str(self.velocity_r)
+        return "Measurement taken " + self.date.strftime("%m/%d/%Y, %H:%M:%S:%f") + " - velocity_f: " + str(self.velocity_f) + " - velocity_r: " + str(self.velocity_r)
+
     def __repr__(self):
         return str(self)
 
-    def storeInRedis(self):
-        with client:
-            client.xadd(stream_name, {"date": str(self.date), "velocity_f": str(self.velocity_f),  "velocity_r": str(self.velocity_r)})
+class MeasurementStore(object):
+    def __new__(cls, redisClient: object, streamName: str):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(MeasurementStore, cls).__new__(cls)
+        return cls.instance
 
-def simulateMeasurement():
-    num_r = np.random.default_rng().normal(mean_velocity_r, 0.01, size=None)
-    num_f = np.random.uniform(low=max(mean_velocity_f, num_r), high=min(mean_velocity_f, num_r), size=None)
-    m = Measurement(num_f, num_r)
-    return m
+    def __init__(self, redisClient: object, streamName: str):
+        self._redisClient = redisClient
+        self._streamName = streamName
+
+    def store(self, m: Measurement):
+        with self._redisClient:
+            self._redisClient.xadd(self._streamName, {"date": str(m.date), "velocity_f": str(m.velocity_f),
+                                                      "velocity_r": str(m.velocity_r)})
+            return True
+        return False
+
+    def getLast(self, count: int):
+        with self._redisClient:
+            l = self._redisClient.xlen(self._streamName)
+
+            if l > 0:
+                values = self._redisClient.xrevrange(self._streamName, min='-', max='+',
+                                                     count=min(l, count))  # getting the last elements from stream
+                values.reverse()  # reversing for better plot
+                return values
+        return []
 
 
-def simulateRuns(runs: int, delay_ms: int):
 
+# custom thread to run simulation and store to Redis
+class SimulationThread(Thread):
+    # constructor
+    def __init__(self, delay: float):
+        # execute the base constructor
+        Thread.__init__(self)
+        # set the delay for run
+        self.delay = delay
+        # set a default value
+        self.value = None
+
+    def simulateMeasurement(self):
+        num_r = np.random.default_rng().normal(mean_velocity_r, 0.01, size=None)
+        num_f = np.random.uniform(low=max(mean_velocity_f, num_r), high=min(mean_velocity_f, num_r), size=None)
+        m = Measurement(num_f, num_r)
+        return m
+
+    # function executed in a new thread
+    def run(self):
+        # block for a moment
+        time.sleep(self.delay)
+        # store data in an instance variable
+        self.value = self.simulateMeasurement()
+
+def simulateRuns(runs: int, delay_ms: int, s: MeasurementStore):
     for i in range(runs):
-        t = Timer(delay_ms/1000, simulateMeasurement, args=())
+        t = SimulationThread(delay_ms / 1000)
         t.start()
         t.join()
+        s.store(t.value)
 
 
-def viewRuns(runs: int):
+def monitorRuns(runs: int, s: MeasurementStore, refresh_ms: int):
     labels = []
     velocities_f = []
     velocities_r = []
     slip = []
 
+    values = s.getLast(runs)
+    for v in values:
+        id, data = v
+    #
+        labels.append(datetime.strptime(data.get('date'), "%Y-%m-%d %H:%M:%S.%f"))
+        velocities_f.append(float(data.get('velocity_f')))
+        velocities_r.append(float(data.get('velocity_r')))
+
+        s = 1 if float(data.get('velocity_f')) < float(data.get('velocity_r')) else 0
+        slip.append(s)
+
+    title = 'Last Runs'
+    os.system('cls' if os.name == 'nt' else 'clear')
+    plt.clt()
+    plt.clf()
+    #
+    #todo make dates used in visualizations
+    dates = plt.datetimes_to_string(labels)
+    plt.plot(velocities_f, label="f", yside="left")
+    plt.plot(velocities_r, label="r", yside="left")
+    plt.scatter(slip, label="slip", yside="right")
+
+    #
+    plt.interactive(True)
+    plt.show()
+
+    time.sleep(refresh_ms/1000)
+
+
+def main():
+    client = connector.connect()
     with client:
-        l = client.xlen(stream_name)
-        if l > 0:
-            values = client.xrevrange(stream_name, min='-', max='+', count=min(l, runs)) #getting the last elements from stream
-            values.reverse() # reversing for better plot
-            for v in values:
-                id, data = v
+        client.ping()
+        store = MeasurementStore(client, stream_name)
+        test = Timer(1, simulateRuns, args=(500, 200, store))
+        test.start()
 
-                labels.append(datetime.strptime(data.get('date'), "%Y-%m-%d %H:%M:%S.%f"))
-                velocities_f.append(float(data.get('velocity_f')))
-                velocities_r.append(float(data.get('velocity_r')))
+        try:
+            while True:
+                monitorRuns(100, store, 1000)
 
-                s = 1 if float(data.get('velocity_f'))<float(data.get('velocity_r')) else 0
-                slip.append(s)
+        except KeyboardInterrupt:
+            pass
 
-            title = 'Last Runs'
-            os.system('cls' if os.name == 'nt' else 'clear')
-            plt.clt()
-            plt.clf()
-            #
-
-            dates = plt.datetimes_to_string(labels)
-            plt.plot(velocities_f, label="f",  yside = "left")
-            plt.plot(velocities_r, label="r",  yside = "left")
-            plt.scatter(slip, label="slip",  yside = "right")
-
-            #
-            plt.interactive(True)
-            plt.show()
-
-            time.sleep(1)
-
-
-test = Timer(1, simulateRuns, args=(500, 200))
-test.start()
-
-try:
-    while True:
-        viewRuns(100)
-
-except KeyboardInterrupt:
-    pass
+if __name__ == "__main__":
+    main()
 
 
 
